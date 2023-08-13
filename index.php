2b60c534f40ee0d996783efd87f0081cfa17c06b
christos-golsouzidis <chr.golsou@gmail.com>
2023-08-13 20:29:23 +0200

    48 test

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <h1>Welcome to my guessing game</h1>
     <p>
         <?php
-            $target = 49;
+            $target = 48;
             if (!isset($_GET['guess'])) {echo "Missing guess parameter";}
             else if (strlen($_GET['guess']) < 1) {echo "Your guess is too short";}
             else if (!is_numeric($_GET['guess'])) {echo "Your guess is not a number";}
